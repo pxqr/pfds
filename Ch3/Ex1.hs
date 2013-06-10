b6cbaa61197dcc708196f9f603ec448ccbd453bb
Sam T <pxqr.sta@gmail.com>
2013-06-10 09:15:50 +0400

    + Ex3.2 and Ex3.3.

--- Ch3/Ex1.hs
+++ Ch3/Ex1.hs
@@ -2,25 +2,36 @@
 module Ch3.Ex1 where
 
 import Control.Applicative hiding (empty)
+import Data.List as L hiding (insert)
 import Data.Monoid
+import Data.Maybe
 import Test.QuickCheck
 
 
 type Rank = Int
 
 data Heap α = Nil
-            | Bin Rank α (Heap α) (Heap α)
+            | Bin {-# UNPACK #-} !Rank α (Heap α) (Heap α)
               deriving Show
 
 instance Ord α => Monoid (Heap α) where
   mempty  = empty
   mappend = merge
+  mconcat = merges
 
 empty :: Heap α
 empty = Nil
 
 singleton :: α → Heap α
 singleton x = Bin 1 x Nil Nil
+
+null :: Heap α → Bool
+null Nil = True
+null _   = False
+
+size :: Heap α → Int
+size  Nil          = 0
+size (Bin _ _ l r) = succ (size l + size r)
 
 
 merge :: Ord α => Heap α → Heap α → Heap α
@@ -33,6 +44,20 @@ merge h1@(Bin _ e1 l1 r1) h2@(Bin _ e2 l2 r2)
 insert :: Ord α => α → Heap α → Heap α
 insert = merge . singleton
 
+{- Ex3.2 standalone insert -}
+insert' :: Ord α => α → Heap α → Heap α
+insert' x = go
+  where
+    go    Nil = singleton x
+    go h@(Bin rk e l r)
+      |  x <= e   = Bin (succ rk) x h Nil
+      | otherwise = bin e l (go r)
+
+insertMin :: a -> Heap a -> Heap a
+insertMin x    Nil          = singleton x
+insertMin x h@(Bin r _ _ _) = Bin (succ r) x h Nil
+
+
 findMin :: Ord α => Heap α → Maybe α
 findMin  Nil          = Nothing
 findMin (Bin _ x _ _) = Just x
@@ -42,7 +67,19 @@ deleteMin  Nil          = Nil
 deleteMin (Bin _ _ l r) = merge l r
 
 fromList :: Ord α => [α] → Heap α
-fromList = foldr insert empty
+fromList = L.foldr insert empty
+
+{- Ex3.3 O(n) fromList -}
+fromList' :: Ord α => [α] → Heap α
+fromList' = merges . map singleton
+
+merges :: Ord α => [Heap α] → Heap α
+merges = head . head . dropWhile ((> 1) . length) . iterate go
+  where
+    go [a, b] = [merge a b]
+    go (a : b : xs) = merge a b : go xs
+    go [x]    = [x]
+    go []     = [empty]
 
 rank :: Heap α → Rank
 rank  Nil          = 0
@@ -50,8 +87,8 @@ rank (Bin r _ _ _) = r
 
 bin :: α → Heap α → Heap α → Heap α
 bin x l r
-  | rank l >= rank r = Bin (rank l + 1) x l r
-  |    otherwise     = Bin (rank r + 1) x r l
+  | rank l >= rank r = Bin (succ (rank l)) x l r
+  |    otherwise     = Bin (succ (rank r)) x r l
 
 
 instance (Arbitrary α, Ord α) => Arbitrary (Heap α) where
